6768950075527e3aa8ec5645db23c6a4b9cf410e
Lauro C Oliveira <lauro@hostcert.com.br>
2022-03-30 11:46:08 -0300

    Adicionado generic Rest notifier

---
+++ lib/notifiers/generic_rest_and_socket_notifier.dart
@@ -0,0 +1,114 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_toolbox/notifiers/generic_map_notifier.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/status.dart' as status;
+import '../utils/static_methods.dart';
+
+class GenericRestAndSocketNotifier extends GenericMapNotifier {
+  GenericRestAndSocketNotifier({required this.keyName, required this.baseSocketUrl, required this.baseRestUrl, required this.authHeaders, required this.ssoToken})
+      : super(keyName: keyName);
+
+  final String keyName;
+  final Map<String, String> authHeaders;
+  final String ssoToken;
+  final String baseRestUrl;
+  final String baseSocketUrl;
+  WebSocketChannel? _ws_channel;
+  StreamSubscription? _channelListener;
+  Stream? stream;
+  Map<String, dynamic> _last_data = {};
+  Map<String, dynamic> get last_data => _last_data;
+  bool _changeSelection = false;
+  bool get changeSelection => _changeSelection;
+  List<String> _inbound_data = [];
+  bool in_dispose = false;
+  int get totalUpdates => _inbound_data.length;
+  Map<int, int> statusCodesByPage = {};
+
+  List<int> _downloadedPages = [];
+
+  Future<void> selectionChanged() async {
+    _changeSelection = false;
+  }
+
+  int _activePage = 1;
+  int get activePage => _activePage;
+  int get totalPages => throw UnimplementedError("Metodo para informar total de paginas");
+  int get totalResults => throw UnimplementedError("Metodo para informar total de resultados");
+  bool get isReady => throw UnimplementedError("Propriedade para informar da inicializacao");
+
+  Future<void> onRequestPrevPage() async {
+    onRequestPage(_activePage - 1, false);
+  }
+
+  Future<void> onRequestNextPage() async {
+    onRequestPage(_activePage + 1, false);
+  }
+
+  // dynamic getById(int id) {
+  //   return _records.firstWhere((record) => record.id == id);
+  // }
+
+  Future<void> onInit() async {
+    throw UnimplementedError("Metodo para inicializar o objeto");
+  }
+
+  Future<void> onRebuildState(String newData) async {
+    throw UnimplementedError("Metodo para reconstruir o state nao implementado");
+  }
+
+  Future<void> onParseCollection(int page, String collection) async {
+    throw UnimplementedError("Metodo para processar uma novacolecao nao implementado");
+  }
+
+  Future<void> onRequestPage(int page, bool waitfor) async {
+    if (!_downloadedPages.contains(page)) {
+      _downloadedPages.add(page);
+      Uri url = Uri.parse("${baseRestUrl}?page=${page}");
+      final request = await StaticMethods.requestGet(url, authHeaders);
+      statusCodesByPage.addAll({page: request.statusCode});
+      notifyListeners();
+      if (request.statusCode == 200) {
+        if (waitfor) {
+          await onParseCollection(page, utf8.decode(request.bodyBytes));
+          _activePage = page;
+        } else {
+          onParseCollection(page, utf8.decode(request.bodyBytes)).then((_) => {
+                _activePage = page,
+                notifyListeners(),
+              });
+        }
+      }
+    } else {
+      _activePage = page;
+      notifyListeners();
+    }
+  }
+
+  Future<void> closeWs() async {
+    _ws_channel?.sink.close(1000);
+  }
+
+  @override
+  void dispose() {
+    in_dispose = true;
+    closeWs().then((value) => {
+          super.dispose(),
+        });
+  }
+
+  Future<void> sendMessage(String msg) async {
+    _ws_channel?.sink.add(msg);
+  }
+
+  Future<void> onStartWs() async {
+    final url = Uri.parse("${baseSocketUrl}?sso=$ssoToken");
+    _ws_channel = WebSocketChannel.connect(url);
+    stream = _ws_channel?.stream;
+    stream?.listen((message) {
+      onRebuildState(message);
+    });
+  }
+}
--- lib/utils/static_methods.dart
+++ lib/utils/static_methods.dart
@@ -7,28 +7,32 @@ import 'package:http/http.dart' as http;
 import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart';
 
-
-
 class StaticMethods {
-
-
-  static Future<String?> getDeviceID() async {
-
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-
-
-    if (Platform.isIOS) {
-      IosDeviceInfo iosDeviceInfo = await deviceInfo.iosInfo;
-      return iosDeviceInfo.identifierForVendor;
+  static Future<http.Response> requestDelete(Uri url, Map<String, String> requestHeaders) async {
+    http.Response resposta = http.Response("", 408);
+    if (!kReleaseMode) {
+      print(url);
     }
-
-    if(Platform.isAndroid){
-      AndroidDeviceInfo androidDeviceInfo = await deviceInfo.androidInfo;
-      return androidDeviceInfo.androidId; // unique ID on Android
+    try {
+      resposta = await http.Client().delete(url, headers: requestHeaders).timeout(const Duration(seconds: 240));
+      if (!kReleaseMode) {}
+      return resposta;
+    } on TimeoutException catch (e) {
+      if (!kReleaseMode) {
+        print(e);
+      }
+      return resposta;
+    } on SocketException catch (e) {
+      if (!kReleaseMode) {
+        print(e);
+      }
+      return resposta;
+    } catch (e) {
+      if (!kReleaseMode) {
+        print(e);
+      }
+      return resposta;
     }
-
-    return "0-0-0-0-";
-
   }
 
   static Future<http.Response> requestGet(Uri url, Map<String, String> requestHeaders) async {
@@ -38,9 +42,7 @@ class StaticMethods {
     }
     try {
       resposta = await http.Client().get(url, headers: requestHeaders).timeout(const Duration(seconds: 240));
-      if (!kReleaseMode) {
-        print(resposta.body);
-      }
+      if (!kReleaseMode) {}
       return resposta;
     } on TimeoutException catch (e) {
       if (!kReleaseMode) {
@@ -60,17 +62,14 @@ class StaticMethods {
     }
   }
 
-  static Future<http.Response> requestPost(
-      Uri url, Map<String, String> requestBody, Map<String, String> requestHeaders) async {
+  static Future<http.Response> requestPatch(Uri url, Map<String, String> requestBody, Map<String, String> requestHeaders) async {
     if (!kReleaseMode) {
       print(url);
       print(requestBody);
     }
     http.Response resposta = http.Response("", 408);
     try {
-      resposta = await http.Client()
-          .post(url, headers: requestHeaders, body: jsonEncode(requestBody))
-          .timeout(const Duration(seconds: 240));
+      resposta = await http.Client().patch(url, headers: requestHeaders, body: jsonEncode(requestBody)).timeout(const Duration(seconds: 240));
       if (!kReleaseMode) {
         print(resposta.body);
       }
@@ -94,19 +93,54 @@ class StaticMethods {
     }
   }
 
-  static Future<http.Response> submitForm(
-      Uri url, Map<String, String> requestBody, Map<String, String> requestHeaders) async {
-    final resposta = await http.Client()
-        .post(url, headers: requestHeaders, body: jsonEncode(requestBody), encoding: Encoding.getByName("utf-8"));
-    return resposta;
+  static Future<http.Response> requestPut(Uri url, Map<String, String> requestBody, Map<String, String> requestHeaders) async {
+    if (!kReleaseMode) {
+      print(url);
+      print(requestBody);
+    }
+    http.Response resposta = http.Response("", 408);
+    try {
+      resposta = await http.Client().put(url, headers: requestHeaders, body: jsonEncode(requestBody)).timeout(const Duration(seconds: 240));
+      if (!kReleaseMode) {
+        print(resposta.body);
+      }
+      return resposta;
+    } on TimeoutException catch (e) {
+      if (!kReleaseMode) {
+        print(e);
+      }
+
+      return resposta;
+    } on SocketException catch (e) {
+      if (!kReleaseMode) {
+        print(e);
+      }
+      return resposta;
+    } catch (e) {
+      if (!kReleaseMode) {
+        print(e);
+      }
+      return resposta;
+    }
   }
 
+  static Future<http.Response> requestPost(Uri url, Map<String, String> requestBody, Map<String, String> requestHeaders) async {
+    if (!kReleaseMode) {}
+    http.Response resposta = http.Response("", 408);
+    try {
+      resposta = await http.Client().post(url, headers: requestHeaders, body: jsonEncode(requestBody)).timeout(const Duration(seconds: 240));
+      if (!kReleaseMode) {}
+      return resposta;
+    } on TimeoutException catch (e) {
+      if (!kReleaseMode) {}
 
-
-  static Future<void> unFocus(BuildContext context) async {
-    FocusScopeNode currentFocus = FocusScope.of(context);
-    if (!currentFocus.hasPrimaryFocus) {
-      currentFocus.unfocus();
+      return resposta;
+    } on SocketException catch (e) {
+      if (!kReleaseMode) {}
+      return resposta;
+    } catch (e) {
+      if (!kReleaseMode) {}
+      return resposta;
     }
   }
 }
