ecae214c80f742c9f4e4cca64777475fe18e4056
Lauro C Oliveira <lauro@hostcert.com.br>
2022-03-30 19:55:25 -0300

    removido device_plus

--- lib/utils/static_methods.dart
+++ lib/utils/static_methods.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:io' show Platform;
